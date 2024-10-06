667b69b86352a64237634a6d73382f72ff0dfecf
Torin Etheridge <torinriley220@gmail.com>
2024-10-06 18:07:37 -0400

    Update TemplateJanx.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateJanx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateJanx.java
@@ -7,86 +7,114 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//ftclib library
-public class TemplateJanx {
-    public DcMotorEx fr = null;
-    public DcMotorEx br = null;
-    public DcMotorEx fl = null;
-    public DcMotorEx bl = null;
+// Beginner-friendly FTC Robot Template for "Error"
+public class Error {
+    // Motors for drive train
+    public DcMotorEx frontRightMotor = null;
+    public DcMotorEx backRightMotor = null;
+    public DcMotorEx frontLeftMotor = null;
+    public DcMotorEx backLeftMotor = null;
 
-    public Servo lc = null;
-    public Servo rc = null;
-    public DcMotorEx nod = null;
+    // Servos for the claw mechanism
+    public Servo leftClaw = null;
+    public Servo rightClaw = null;
+    
+    // Motor for controlling the claw nodding movement
+    public DcMotorEx clawNodMotor = null;
 
-    public DcMotorEx ext = null;
-    public DcMotorEx turn = null;
+    // Motors for the arm mechanism
+    public DcMotorEx armExtendMotor = null;
+    public DcMotorEx armTurnMotor = null;
+    public DcMotorEx leftArmMotor = null;
+    public DcMotorEx rightArmMotor = null;
 
-    public DcMotorEx sl = null;
-    public DcMotorEx sr = null;
-
+    // Hardware Map
     HardwareMap hwMap = null;
 
-
-    //clockwise from front right
-    public TemplateJanx(HardwareMap h) {
-        hwMap = h;
+    // Constructor to initialize hardware map
+    public Error(HardwareMap hardwareMap) {
+        hwMap = hardwareMap;
     }
 
-    public void wheelInit(String frontRight, String backRight, String backleft, String frontLeft) {
-        //when you make this in teleop or autonomous, you can import hardware.DcMotor with the keyword hardwareMap
-        fr = hwMap.get(DcMotorEx.class, frontRight);
-        br = hwMap.get(DcMotorEx.class, backRight);
-        fl = hwMap.get(DcMotorEx.class, frontLeft);
-        bl = hwMap.get(DcMotorEx.class, backleft);
+    // Method to initialize the drive train motors
+    public void initializeDriveTrain(String frontRight, String backRight, String backLeft, String frontLeft) {
+        frontRightMotor = hwMap.get(DcMotorEx.class, frontRight);
+        backRightMotor = hwMap.get(DcMotorEx.class, backRight);
+        frontLeftMotor = hwMap.get(DcMotorEx.class, frontLeft);
+        backLeftMotor = hwMap.get(DcMotorEx.class, backLeft);
 
-        fr.setDirection(DcMotor.Direction.FORWARD);
-        br.setDirection(DcMotor.Direction.FORWARD);
-        fl.setDirection(DcMotor.Direction.REVERSE);
-        bl.setDirection(DcMotor.Direction.REVERSE);
-        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT); //should it be brake?
-        bl.setPower(0);
-        fl.setPower(0);
-        br.setPower(0);
-        fr.setPower(0);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        setMotorMode(frontRightMotor, DcMotor.RunMode.RUN_USING_ENCODER);
+        setMotorMode(backRightMotor, DcMotor.RunMode.RUN_USING_ENCODER);
+        setMotorMode(frontLeftMotor, DcMotor.RunMode.RUN_USING_ENCODER);
+        setMotorMode(backLeftMotor, DcMotor.RunMode.RUN_USING_ENCODER);
+
+        setMotorZeroPowerBehavior(frontRightMotor, DcMotor.ZeroPowerBehavior.BRAKE);
+        setMotorZeroPowerBehavior(backRightMotor, DcMotor.ZeroPowerBehavior.BRAKE);
+        setMotorZeroPowerBehavior(frontLeftMotor, DcMotor.ZeroPowerBehavior.BRAKE);
+        setMotorZeroPowerBehavior(backLeftMotor, DcMotor.ZeroPowerBehavior.BRAKE);
+
+        stopAllMotors();
     }
 
-    public void clawInit(String leftClaw, String rightClaw, String nodder) {
-        nod = hwMap.get(DcMotorEx.class, nodder);
-        nod.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        nod.setDirection(DcMotor.Direction.FORWARD);
-        lc = hwMap.get(Servo.class, leftClaw);
-        rc = hwMap.get(Servo.class, rightClaw);
+    // Method to initialize the claw mechanism
+    public void initializeClaw(String leftClawName, String rightClawName, String nodMotorName) {
+        clawNodMotor = hwMap.get(DcMotorEx.class, nodMotorName);
+        clawNodMotor.setDirection(DcMotor.Direction.FORWARD);
+        clawNodMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftClaw = hwMap.get(Servo.class, leftClawName);
+        rightClaw = hwMap.get(Servo.class, rightClawName);
     }
 
-    public void armInit(String armextend, String armTurn, String left, String right) {
-        sl = hwMap.get(DcMotorEx.class, left);
-        sr = hwMap.get(DcMotorEx.class, right);
-        turn = hwMap.get(DcMotorEx.class, armTurn);
+    // Method to initialize the arm mechanism
+    public void initializeArm(String extendMotorName, String turnMotorName, String leftArmName, String rightArmName) {
+        armExtendMotor = hwMap.get(DcMotorEx.class, extendMotorName);
+        armTurnMotor = hwMap.get(DcMotorEx.class, turnMotorName);
+        leftArmMotor = hwMap.get(DcMotorEx.class, leftArmName);
+        rightArmMotor = hwMap.get(DcMotorEx.class, rightArmName);
 
-        turn.setDirection(DcMotor.Direction.FORWARD);
-        turn.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        turn.setPower(0);
+        armTurnMotor.setDirection(DcMotor.Direction.FORWARD);
+        armTurnMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armTurnMotor.setPower(0);
 
-        sl.setDirection(DcMotor.Direction.FORWARD);
-        sl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        sl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        sl.setPower(0);
-
-        sr.setDirection(DcMotor.Direction.REVERSE);
-        sr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        sr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        sr.setPower(0);
+        initializeArmMotor(leftArmMotor, DcMotor.Direction.FORWARD);
+        initializeArmMotor(rightArmMotor, DcMotor.Direction.REVERSE);
     }
 
+    // Helper method to set motor mode
+    private void setMotorMode(DcMotorEx motor, DcMotor.RunMode mode) {
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(mode);
+    }
+
+    // Helper method to set motor zero power behavior
+    private void setMotorZeroPowerBehavior(DcMotorEx motor, DcMotor.ZeroPowerBehavior behavior) {
+        motor.setZeroPowerBehavior(behavior);
+    }
+
+    // Helper method to initialize arm motors
+    private void initializeArmMotor(DcMotorEx motor, DcMotor.Direction direction) {
+        motor.setDirection(direction);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setPower(0);
+    }
+
+    // Method to stop all motors
+    public void stopAllMotors() {
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        armExtendMotor.setPower(0);
+        armTurnMotor.setPower(0);
+        leftArmMotor.setPower(0);
+        rightArmMotor.setPower(0);
+        if (clawNodMotor != null) clawNodMotor.setPower(0);
+    }
 }
